5418e7be8f840ede28deb1c27ce2ecb53a4b052c
Carlos Eduardo <edu.carlos.cec@gmail.com>
2023-12-11 00:26:57 -0300

    Toast for password and email

--- src/main.ts
+++ src/main.ts
@@ -10,10 +10,13 @@ import "primevue/resources/themes/saga-blue/theme.css"; // Theme
 import "primevue/resources/primevue.min.css"; // Core CSS
 import "primeicons/primeicons.css"; // Icons
 import "primeflex/primeflex.css"; // PrimeVue 
+import 'primevue/resources/primevue.min.css';
 
 // Components
 import Button from 'primevue/button'
 import InputText from 'primevue/inputtext';
+import Toast from 'primevue/toast';
+import ToastService from 'primevue/toastservice';
 // ------------------------------------------------------------------
 
 
@@ -21,6 +24,8 @@ createApp(App)
 .use(store)
 .use(router)
 .use(PrimeVue)
+.use(ToastService)
 .component('ButtonVue', Button)
 .component('InputTextVue', InputText)
+.component('ToastVue', Toast)
 .mount('#app')
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,4 +1,5 @@
 <template>
+    <ToastVue/>
     <div class="
         w-full max-w-full 
         min-h-full h-full p-3
@@ -32,28 +33,26 @@
         </div>
         
 
-        <div class="w-100 flex flex-column align-items-center justify-content-start">
-            <div class="w-3">
-                <div class="flex flex-column gap-2 mb-5">
-                    <label for="username">E-mail</label>
-                    <InputTextVue type="text" placeholder="" size="large" v-model="user.email" />
-                    <small id="username-help">Enter your username to reset your password.</small>
-                </div>
-
-                <div class="flex flex-column gap-3 mb-5">
-                    <label for="username">Password</label>
-                    <InputTextVue type="text" size="large" v-model="user.password" />
-                    <small id="username-help">Enter your username to reset your password.</small>
-                </div>
-
-                <div class="flex flex-column gap-2 mb-3">
-                    <ButtonVue size="large" label="Entrar" severity="danger" />
-                </div>
-                
-                <router-link to="/reset-password" class="text-white no-underline font-bold">
-                    Esqueceu sua senha?
-                </router-link>
+        <div class="w-4 flex flex-column align-items-center justify-content-start m-auto">
+            <div class="flex flex-column gap-2 mb-5 w-full">
+                <label for="username">E-mail</label>
+                <InputTextVue id="" type="text" placeholder="" size="large" v-model="user.email" />
+                <small id="username-help">Enter your username to reset your password.</small>
             </div>
+
+            <div class="flex flex-column gap-3 mb-5 w-full">
+                <label for="username">Password</label>
+                <InputTextVue type="text" size="large" v-model="user.password" />
+                <small id="username-help">Enter your username to reset your password.</small>
+            </div>
+
+            <div class="flex flex-column gap-2 mb-3 w-full">
+                <ButtonVue size="large" label="Entrar" severity="danger" @click="validate"/>
+            </div>
+            
+            <router-link to="/reset-password" class="text-white no-underline font-bold">
+                Esqueceu sua senha?
+            </router-link>
         </div>
     </div>
 </template>
@@ -67,9 +66,19 @@ export default defineComponent({
         return {
             user: {
                 email: null,
-                password: null
+                password: null,
             }
         }
+    },
+    methods: {
+        validate(){
+            if(
+                !this.user.email ||
+                !this.user.password
+            ){
+                this.$toast.add({ severity: 'error', summary: 'Error Message', detail: 'E-mail e Password são necessários.', life: 3000 });
+            } 
+        },
     }
 });
 </script>
