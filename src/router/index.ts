b806361b2d8426b1f06a9c49b5465afcd3601065
Carlos Eduardo <edu.carlos.cec@gmail.com>
2023-12-11 16:02:33 -0300

    Creating first inputs in register user

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import LoginView from "@/views/LoginView.vue"
 import SplashView from "@/views/SplashView.vue"
+import RegisterUserView from "@/views/RegisterUserView.vue"
 
 const routes = [
   {
@@ -12,6 +13,11 @@ const routes = [
     path: '/login',
     name: 'LoginView',
     component: LoginView
+  },
+  {
+    path: '/register-user',
+    name: 'RegisterUserView',
+    component: RegisterUserView
   }
 ]
 
