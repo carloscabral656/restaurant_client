b2a3e7395e2bd01c2747677820e7c8e4992e9a9e
Carlos Eduardo <edu.carlos.cec@gmail.com>
2023-12-18 08:19:36 -0300

    Updating login css

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -10,7 +10,7 @@
         <div class="flex flex-row flex-wrap justify-content-between">
             <div class="flex align-items-center justify-content">
                 <router-link to="/">
-                    <ButtonVue icon="pi pi-arrow-left" rounded/>
+                    <ButtonVue icon="pi pi-arrow-left" severity="danger"  rounded/>
                 </router-link>
             </div>
 
@@ -28,42 +28,58 @@
             flex align-items-center justify-content-start
             text-3xl font-bold
         ">
-            Acesse seus restaurantes prediletos {{isAuthenticated}}
+            Acesse seus restaurantes prediletos
         </div>
         
 
-        <div class="
-                flex flex-column align-items-center justify-content-start m-auto">
+        <div class="flex flex-column align-items-center justify-content-start m-auto">
             
             <div class="
                 flex flex-column
                 sm:w-100
-                md:w-100
+                md:w-50
                 lg:w-4
                 xl:w-4
                 gap-2 mb-5 w-full
             ">
                 <label for="email">E-mail</label>
-                <InputTextVue id="email" size="large" v-model="user.email" />
+                <InputTextVue id="email" size="large" v-model="user.email" inputClass="w-100"/>
                 <small id="email">Enter your username to reset your password.</small>
             </div>
 
             <div class="
                 flex flex-column
-                sm:w-100 
+                sm:w-100
+                md:w-50
+                lg:w-4
+                xl:w-4 
                 gap-2 mb-5 w-full">
                 <label for="password">Password</label>
                 <PasswordVue id="password" inputClass="w-full" size="large" :feedback="false" v-model="user.password" />
                 <small id="password">Enter your username to reset your password.</small>
             </div>
 
-            <div class="w-4 flex flex-column gap-2 mb-3 w-full">
+            <div class="
+                flex flex-column
+                sm:w-100
+                md:w-50
+                lg:w-4
+                xl:w-4
+                gap-2 mb-5 w-full">
                 <ButtonVue size="large" label="Entrar" severity="danger" @click="validate"/>
             </div>
             
-            <router-link to="/reset-password" class="w-4 text-white no-underline font-bold">
-                Esqueceu sua senha?
-            </router-link>
+            <div class="
+                sm:w-100
+                md:w-50
+                lg:w-4
+                xl:w-4
+                text-center
+            ">
+                <router-link to="/reset-password" class=" text-white no-underline font-bold">
+                    Esqueceu sua senha?
+                </router-link>
+            </div>
         </div>
     </div>
 </template>
