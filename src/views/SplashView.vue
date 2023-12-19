18d34a70cc3c7978885c24df84d71e29ad0a2b3a
Carlos Eduardo <edu.carlos.cec@gmail.com>
2023-12-18 22:09:41 -0300

    Size button to login

--- src/views/SplashView.vue
+++ src/views/SplashView.vue
@@ -1,12 +1,12 @@
 <template>
-    <div class="h-full min-h-screen p-4 surface-900 text-white text-3xl bg-cover opacity-80 md:text-4xl lg:text-4xl xl:text-4xl" style="background-image: url('/splash.jpg');">
+    <div class="flex flex-row align-items-center justify-content-center h-full min-h-screen p-4 surface-900 text-white text-3xl bg-cover opacity-80 md:text-4xl lg:text-4xl xl:text-4xl" style="background-image: url('/splash.jpg');">
         <div class="flex flex-row align-items-center justify-content-center">
             <h1>Quem tem boca</h1>
             <img src="/boca.png" class="col-6 h-10rem w-10rem"/>
         </div>
-        <div class="absolute bottom-0 right-0 mb-2 mr-2">
+        <div class="absolute bottom-0 right-0 mb-4 mr-4">
             <router-link to="/login">
-                <ButtonVue class="" icon="pi pi-arrow-right" severity="danger" rounded/>
+                <ButtonVue class="w-6rem h-6rem" icon="pi pi-arrow-right text-3xl md:text-4xl lg:text-3xl xl:text-3xl" severity="danger" rounded/>
             </router-link>
         </div>
     </div>
